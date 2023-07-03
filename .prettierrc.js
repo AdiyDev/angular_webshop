b60c2a9144950d9ee93e1fe2808b4d2ec784388a
AdiyDev <adiy.work.mail@ya.ru>
2023-07-03 12:56:29 +0300

    feat: init app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  semi: false,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true,
+}
