a48b168268c36071bf386e3cd055ea163b0e0570
AdiyDev <adiy.personal.mail@ya.ru>
2023-07-04 19:44:07 +0300

    feat: add pages product and about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,8 @@
-import { ModalService } from './services/modal.service'
-import { ProductService } from './services/products.service'
-import { Component, OnInit } from '@angular/core'
-import { IProduct } from './models/product'
-import { Observable, tap } from 'rxjs'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'angular webshop'
-  term = ''
-  // products: IProduct[] = []
-  loading = false
-  // products$: Observable<IProduct[]>
-
-  constructor(public productService: ProductService, public modalService: ModalService) {}
-
-  ngOnInit(): void {
-    this.loading = true
-    this.productService.getAll().subscribe(() => (this.loading = false))
-    // this.products$ = this.productService.getAll().pipe(tap({ next: () => (this.loading = false) }))
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,11 @@ import { GlobalErrorComponent } from './components/global-error/global-error.com
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { FilterProductsPipe } from './pipes/filter-products.pipe'
 import { ModalComponent } from './components/modal/modal.component'
-import { CreateProductComponent } from './components/create-product/create-product.component';
+import { CreateProductComponent } from './components/create-product/create-product.component'
 import { FocusDirective } from './directives/focus.directive'
+import { ProductPageComponent } from './pages/product-page/product-page.component'
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component'
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { FocusDirective } from './directives/focus.directive'
     ModalComponent,
     CreateProductComponent,
     FocusDirective,
+    ProductPageComponent,
+    AboutPageComponent,
+    NavigationComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
