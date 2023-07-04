1e190e69fd7f62c2e019fafbfd8c5f2ccd69c32c
AdiyDev <adiy.personal.mail@ya.ru>
2023-07-04 15:11:10 +0300

    feat: add http client module add products service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core'
+import { ProductService } from './services/products.service'
+import { Component, OnInit } from '@angular/core'
 import { IProduct } from './models/product'
-import { products } from './data/products'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular webshop'
-  products: IProduct[] = products
+  products: IProduct[] = []
+  loading = false
+
+  constructor(private ProductService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loading = true
+    this.ProductService.getAll().subscribe((prod) => {
+      this.products = prod
+      this.loading = false
+    })
+  }
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Observable, delay } from 'rxjs'
+import { IProduct } from '../models/product'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<IProduct[]> {
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products', {
+        // params: new HttpParams().append('limit', 5),
+        // params: new HttpParams({
+        //   fromString: 'limit=5',
+        // }),
+        params: new HttpParams({
+          fromObject: { limit: 4 },
+        }),
+      })
+      .pipe(delay(1500))
+  }
+}
