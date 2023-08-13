e51b0d1b38641855a939b7dc41a35d17f763dac8
AdiyDev <adiy.personal.mail@ya.ru>
2023-08-13 23:35:22 +0300

    chore: fix unused variables and imports

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -3,25 +3,19 @@ import { ModalService } from 'src/app/services/modal.service'
 
 import { Component, OnInit } from '@angular/core'
 
-import { Observable, tap } from 'rxjs'
-
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.scss'],
 })
 export class ProductPageComponent implements OnInit {
   title = 'angular webshop'
   term = ''
-  // products: IProduct[] = []
   loading = false
-  // products$: Observable<IProduct[]>
 
   constructor(public productService: ProductService, public modalService: ModalService) {}
 
   ngOnInit(): void {
     this.loading = true
     this.productService.getAll().subscribe(() => (this.loading = false))
-    // this.products$ = this.productService.getAll().pipe(tap({ next: () => (this.loading = false) }))
   }
 }
